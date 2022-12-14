d35af3a00eb73189a661bee05cc7ef5d99a024b6
Mateusz Miszczak <mthw.miszczak@gmail.com>
2022-12-14 14:22:26 +0100

    getHeroStats function done so get random hero button now gives stats of the super hero as well, floating border that had no img removed, runApp function added, modified html and css a bit more

--- script.js
+++ script.js
@@ -5,26 +5,26 @@
 const SUPERHERO_TOKEN = "650477136803829";
 const BASE_URL = `https://superheroapi.com/api.php/${SUPERHERO_TOKEN}`;
 const btnNewHero = document.getElementById("newHero");
-const heroImageDiv = document.getElementById("heroImg");
+const heroImageDiv = document.getElementById("heroImgDiv");
 const searchButton = document.getElementById("searchBtn");
 const searchInput = document.getElementById("searchInput");
 const statsDiv = document.getElementById("stats");
+const heroImg = document.getElementsByClassName("heroImg");
+const allImgs = document.querySelectorAll("img");
+
+const changeBorderOfImgsToNone = () => {
+  allImgs.forEach((img) => {
+    img.style.border = "none";
+  });
+};
 
 const getRandomSuperHero = (id) => {
   fetch(`${BASE_URL}/${id}/image`)
     .then((response) => response.json())
     .then((json) => {
-      console.log(json);
       const name = `<h2> ${json.name} </h2>`;
-      // const int = `<h3>Intelligence: ${json.powerstats.intelligence} </h3>`;
-      // const combat = `<h3>Combat: ${json.powerstats.combat} </h3>`;
-      // const dur = `<h3>Durability: ${json.powerstats.durability} </h3>`;
-      // const power = `<h3>Power: ${json.powerstats.power} </h3>`;
-      // const speed = `<h3>Speed: ${json.powerstats.speed} </h3>`;
-      // const strength = `<h3>Strength: ${json.powerstats.strength} </h3>`;
-
       heroImageDiv.innerHTML = `${name}<img src=${json.url} height=200 width=200 />`;
-      // statsDiv.innerHTML = `${int} ${combat} ${dur} ${power} ${speed} ${strength} `;
+      getHeroStats(id);
     });
 };
 
@@ -38,7 +38,6 @@ const getSearchSuperHero = (name) => {
     .then((response) => response.json())
     .then((json) => {
       const hero = json.results[0];
-      console.log(hero);
       const name = `<h2> ${hero.name} </h2>`;
       const int = `<h3>Intelligence: ${hero.powerstats.intelligence} </h3>`;
       const combat = `<h3>Combat: ${hero.powerstats.combat} </h3>`;
@@ -52,5 +51,26 @@ const getSearchSuperHero = (name) => {
     });
 };
 
-btnNewHero.onclick = () => getRandomSuperHero(generateNewId());
-searchButton.onclick = () => getSearchSuperHero(searchInput.value);
+const getHeroStats = (id) => {
+  fetch(`${BASE_URL}/${id}/powerstats`)
+    .then((response) => response.json())
+    .then((json) => {
+      const int = `<h3>Intelligence: ${json.intelligence} </h3>`;
+      console.log(int);
+      const combat = `<h3>Combat: ${json.combat} </h3>`;
+      const dur = `<h3>Durability: ${json.durability} </h3>`;
+      const power = `<h3>Power: ${json.power} </h3>`;
+      const speed = `<h3>Speed: ${json.speed} </h3>`;
+      const strength = `<h3>Strength: ${json.strength} </h3>`;
+
+      statsDiv.innerHTML = `${int} ${combat} ${dur} ${power} ${speed} ${strength} `;
+    });
+};
+
+const runApp = () => {
+  changeBorderOfImgsToNone();
+  btnNewHero.onclick = () => getRandomSuperHero(generateNewId());
+  searchButton.onclick = () => getSearchSuperHero(searchInput.value);
+};
+
+runApp();
