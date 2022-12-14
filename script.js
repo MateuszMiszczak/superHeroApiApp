a60d54259e69cbd770f5bb73f69200082eda4d29
Mateusz Miszczak <mthw.miszczak@gmail.com>
2022-12-14 12:36:21 +0100

    Corrected JS a bit, added new IDs and classes, styled the app

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ const btnNewHero = document.getElementById("newHero");
 const heroImageDiv = document.getElementById("heroImg");
 const searchButton = document.getElementById("searchBtn");
 const searchInput = document.getElementById("searchInput");
-const heroNameDiv = document.getElementById("heroName");
 const statsDiv = document.getElementById("stats");
 
 const getRandomSuperHero = (id) => {
@@ -16,8 +15,16 @@ const getRandomSuperHero = (id) => {
     .then((response) => response.json())
     .then((json) => {
       console.log(json);
-      heroNameDiv.innerHTML = `${json.name}`;
-      heroImageDiv.innerHTML = `<img src=${json.url} height=200 width=200 />`;
+      const name = `<h2> ${json.name} </h2>`;
+      // const int = `<h3>Intelligence: ${json.powerstats.intelligence} </h3>`;
+      // const combat = `<h3>Combat: ${json.powerstats.combat} </h3>`;
+      // const dur = `<h3>Durability: ${json.powerstats.durability} </h3>`;
+      // const power = `<h3>Power: ${json.powerstats.power} </h3>`;
+      // const speed = `<h3>Speed: ${json.powerstats.speed} </h3>`;
+      // const strength = `<h3>Strength: ${json.powerstats.strength} </h3>`;
+
+      heroImageDiv.innerHTML = `${name}<img src=${json.url} height=200 width=200 />`;
+      // statsDiv.innerHTML = `${int} ${combat} ${dur} ${power} ${speed} ${strength} `;
     });
 };
 
@@ -27,20 +34,21 @@ const generateNewId = () => {
 };
 
 const getSearchSuperHero = (name) => {
-  console.log(searchInput.value);
   fetch(`${BASE_URL}/search/${name}`)
     .then((response) => response.json())
     .then((json) => {
       const hero = json.results[0];
       console.log(hero);
-      heroNameDiv.innerHTML = `${hero.name}`;
-      heroImageDiv.innerHTML = `<img src=${hero.image.url} height=200 width=200 />`;
-      statsDiv.innerHTML = `Intelligence: ${hero.powerstats.intelligence}
-      Combat: ${hero.powerstats.combat}
-      Durability: ${hero.powerstats.durability}
-      Power: ${hero.powerstats.power}
-      Speed: ${hero.powerstats.speed}
-      Strength: ${hero.powerstats.strength}`;
+      const name = `<h2> ${hero.name} </h2>`;
+      const int = `<h3>Intelligence: ${hero.powerstats.intelligence} </h3>`;
+      const combat = `<h3>Combat: ${hero.powerstats.combat} </h3>`;
+      const dur = `<h3>Durability: ${hero.powerstats.durability} </h3>`;
+      const power = `<h3>Power: ${hero.powerstats.power} </h3>`;
+      const speed = `<h3>Speed: ${hero.powerstats.speed} </h3>`;
+      const strength = `<h3>Strength: ${hero.powerstats.strength} </h3>`;
+
+      heroImageDiv.innerHTML = `${name} <img src=${hero.image.url} height=200 width=200 /> `;
+      statsDiv.innerHTML = `${int} ${combat} ${dur} ${power} ${speed} ${strength} `;
     });
 };
 
