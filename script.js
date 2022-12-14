b764ff81762215d91265e070fce0fbffc7e7f263
Mateusz Miszczak <mthw.miszczak@gmail.com>
2022-12-14 11:06:53 +0100

    Searching hero displays his image, name and stats. Random hero generates his image and name

--- script.js
+++ script.js
@@ -4,14 +4,19 @@
 
 const SUPERHERO_TOKEN = "650477136803829";
 const BASE_URL = `https://superheroapi.com/api.php/${SUPERHERO_TOKEN}`;
-const btnNewHero = document.getElementById("new-hero");
-const heroImageDiv = document.getElementById("hero-img");
+const btnNewHero = document.getElementById("newHero");
+const heroImageDiv = document.getElementById("heroImg");
+const searchButton = document.getElementById("searchBtn");
+const searchInput = document.getElementById("searchInput");
+const heroNameDiv = document.getElementById("heroName");
+const statsDiv = document.getElementById("stats");
 
-const getSuperHero = (id, name) => {
+const getRandomSuperHero = (id) => {
   fetch(`${BASE_URL}/${id}/image`)
     .then((response) => response.json())
     .then((json) => {
       console.log(json);
+      heroNameDiv.innerHTML = `${json.name}`;
       heroImageDiv.innerHTML = `<img src=${json.url} height=200 width=200 />`;
     });
 };
@@ -21,8 +26,23 @@ const generateNewId = () => {
   return randomNumber;
 };
 
-const buttonOnclickChangeHero = () => {
-  btnNewHero.onclick = () => getSuperHero(generateNewId());
+const getSearchSuperHero = (name) => {
+  console.log(searchInput.value);
+  fetch(`${BASE_URL}/search/${name}`)
+    .then((response) => response.json())
+    .then((json) => {
+      const hero = json.results[0];
+      console.log(hero);
+      heroNameDiv.innerHTML = `${hero.name}`;
+      heroImageDiv.innerHTML = `<img src=${hero.image.url} height=200 width=200 />`;
+      statsDiv.innerHTML = `Intelligence: ${hero.powerstats.intelligence}
+      Combat: ${hero.powerstats.combat}
+      Durability: ${hero.powerstats.durability}
+      Power: ${hero.powerstats.power}
+      Speed: ${hero.powerstats.speed}
+      Strength: ${hero.powerstats.strength}`;
+    });
 };
 
-buttonOnclickChangeHero();
+btnNewHero.onclick = () => getRandomSuperHero(generateNewId());
+searchButton.onclick = () => getSearchSuperHero(searchInput.value);
